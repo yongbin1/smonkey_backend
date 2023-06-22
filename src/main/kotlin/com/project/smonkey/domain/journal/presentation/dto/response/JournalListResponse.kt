66a51c5096959d7273581b3ded2bea57e91c8120
yongbin1 <rhdydqls1024@naver.com>
2023-06-23 04:54:48 +0900

    :memo: :: 배포

--- src/main/kotlin/com/project/smonkey/domain/journal/presentation/dto/response/JournalListResponse.kt
+++ src/main/kotlin/com/project/smonkey/domain/journal/presentation/dto/response/JournalListResponse.kt
@@ -1,3 +1,5 @@
 package com.project.smonkey.domain.journal.presentation.dto.response
 
-data class JournalListResponse()
+data class JournalListResponse(
+    val list: List<JournalResponse>
+)
--- src/main/kotlin/com/project/smonkey/domain/journal/service/FindAllMonthJournalService.kt
+++ src/main/kotlin/com/project/smonkey/domain/journal/service/FindAllMonthJournalService.kt
@@ -1,4 +1,40 @@
 package com.project.smonkey.domain.journal.service
 
-class FindAllMonthJournalService {
+import com.project.smonkey.domain.journal.domain.repository.JournalRepository
+import com.project.smonkey.domain.journal.presentation.dto.response.JournalListResponse
+import com.project.smonkey.domain.user.facade.UserFacade
+import com.project.smonkey.global.payload.BaseResponse
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+@Service
+class FindAllMonthJournalService(
+    private val userFacade: UserFacade,
+    private val journalRepository: JournalRepository,
+) {
+
+    @Transactional
+    fun execute(
+        month: Int,
+    ): BaseResponse<JournalListResponse> {
+        val user = userFacade.getCurrentUser()
+
+        var days = 0
+        if (month == 2) {
+            days = 27
+        } else {
+            days = if (month < 8) {
+                if (month % 2 == 0) 30 else 31
+            } else {
+                if (month % 2 == 0) 31 else 30
+            }
+        }
+
+        return BaseResponse(
+            status = 200,
+            message = "success find journal data",
+            content = null
+        )
+    }
+
 }
